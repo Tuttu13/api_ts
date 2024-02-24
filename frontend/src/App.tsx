5713f1196f3de6143a33bd06e3e666c9cd49e605
Tuttu_13 <willson30426@yahoo.co.jp>
2024-02-24 15:25:04 +0900

    メイン層　修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,86 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import AppLayout from "./layout/AppLayout";
 import { createTheme } from "./theme";
+import TodoList from "./todo/TodoList";
+import TodoEdit from "./todo/components/TodoEdit";
+import { BackgroundLocation } from "./todo/hooks/useModalRoute";
+import { useTodos } from "./todo/hooks/useTodos";
 
 function App() {
   const theme = createTheme();
+  const { todos, setTodos } = useTodos();
+  const location = useLocation();
+  const background = (location.state as BackgroundLocation)?.background;
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes location={background || location}>
+          <Route path="/" element={<AppLayout />}>
+            <Route
+              index
+              element={
+                <TodoList
+                  todos={todos}
+                  setTodos={setTodos}
+                  filterFunction={(todo) => !todo.done}
+                />
+              }
+            />
+            <Route
+              path="/important"
+              element={
+                <TodoList
+                  todos={todos}
+                  setTodos={setTodos}
+                  filterFunction={(todo) => todo.important}
+                />
+              }
+            />
+            <Route
+              path="/all"
+              element={
+                <TodoList
+                  todos={todos}
+                  setTodos={setTodos}
+                  filterFunction={() => true}
+                />
+              }
+            />
+            <Route
+              path="/complited"
+              element={
+                <TodoList
+                  todos={todos}
+                  setTodos={setTodos}
+                  filterFunction={(todo) => todo.done}
+                />
+              }
+            />
+            <Route
+              path="/todos/:id"
+              element={
+                <TodoList
+                  todos={todos}
+                  setTodos={setTodos}
+                  filterFunction={(todo) => !todo.done}
+                />
+              }
+            />
+          </Route>
+        </Routes>
+        {background && (
           <Routes>
-            <Route path="/" element={<AppLayout />}></Route>
+            <Route
+              path="/todos/:id"
+              element={<TodoEdit todos={todos} setTodos={setTodos} />}
+            />
           </Routes>
-        </ThemeProvider>
-      </BrowserRouter>
+        )}
+      </ThemeProvider>
     </div>
   );
 }
