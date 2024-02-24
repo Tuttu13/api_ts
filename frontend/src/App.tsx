6c7a2aaf6420b3ff35bd51106f1026446b3239c0
Tuttu_13 <willson30426@yahoo.co.jp>
2024-02-24 13:51:25 +0900

    レイアウト　追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import AppLayout from "./layout/AppLayout";
+import { createTheme } from "./theme";
 
 function App() {
+  const theme = createTheme();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<AppLayout />}></Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/layout/AppLayout.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+
+const AppLayout: React.FC = () => {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Sidebar />
+      <Outlet />
+    </Box>
+  );
+};
+
+export default AppLayout;
